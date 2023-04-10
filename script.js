de0eb36828c16d7bbc762fec8a52ee731b6ba18e
painter98 <120320713+painter98@users.noreply.github.com>
2023-04-10 21:07:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let arr = [1, 2, 3, 4, 5];
 function incrementArray(arr) {
   //your code here
+	let ans = [];
+	for(let i=0;i<arr.length;i++){
+		ans[i]=++arr[i];
+	}
+	return ans;
 }
 
 alert(incrementArray(arr));
